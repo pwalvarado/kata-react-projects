1eae47e2ee37c3a770971f46789ceec325ad1a48
Pedro Alvarado <pwalvarado@gmail.com>
2020-01-29 16:23:41 -0500

    wire webpack to render react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+    <h1>Movie list</h1>
+)
+
+ReactDOM.render(<App />, getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+})
+
 module.exports = {
     module: {
         rules: [
@@ -5,9 +12,12 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: '"babel-loader',
+                    loader: 'babel-loader',
                 },
             },
         ],
     },
+    plugins: [
+        htmlPlugin
+    ],
 }
